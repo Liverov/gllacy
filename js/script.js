f41aa5677480dbba96e15ebde620407614ca4966
Anton Liverov <liverov@yandex.ru>
2020-02-04 11:54:38 +0300

    Личный проект: завершаем разметку
    Сделал карточки товаров в соответствии с рекомендациями

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const cards = document.querySelectorAll('.card');
+Array.prototype.forEach.call(cards, card => {
+    let down, up, link = card.querySelector('h3 a');
+    card.style.cursor = 'pointer';
+    card.onmousedown = () => down = +new Date();
+    card.onmouseup = () => {
+        up = +new Date();
+        if ((up - down) < 200) {
+            link.click();
+        }
+    }
+});
