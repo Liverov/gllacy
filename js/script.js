293934b1773d978fd840668c735e1641a9a3a3c4
Anton Liverov <liverov@yandex.ru>
2020-02-07 12:29:30 +0300

    Личный проект: начинаем базовую стилизацию
    Убрал инлайн стили

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 const cards = document.querySelectorAll('.card');
 Array.prototype.forEach.call(cards, card => {
     let down, up, link = card.querySelector('h3 a');
-    card.style.cursor = 'pointer';
     card.onmousedown = () => down = +new Date();
     card.onmouseup = () => {
         up = +new Date();
