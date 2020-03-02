a9629033ce9e36b7a886c6904b4278d3fe0294fa
Anton Liverov <liverov@yandex.ru>
2020-03-02 19:00:10 +0300

    Добавил немножко JS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+var feedback_button = document.querySelector(".feedback-button");
+var modal_feedback = document.querySelector(".modal-feedback");
+var modal_close = document.querySelector(".modal-close");
+
+feedback_button.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    modal_feedback.style.display = "block";
+});
+
+modal_close.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    modal_feedback.style.display = "none";
+});
+
+/* PRODUCT CARD */
 const cards = document.querySelectorAll('.card');
 Array.prototype.forEach.call(cards, card => {
     let down, up, link = card.querySelector('h3 a');
